4b09c42a74a5103f04f6ba2ae0045eeeacebad81
jkintana <jkintana@protonmail.com>
2022-10-06 14:07:09 -0500

    Made a component.

--- src/App.js
+++ src/App.js
@@ -1,49 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+import React from "react";
+import Project from "./Project";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyB582KCnKC92sag4TOFWlk_7tNH1mAK4mk",
-  authDomain: "personal-website-323f6.firebaseapp.com",
-  projectId: "personal-website-323f6",
-  storageBucket: "personal-website-323f6.appspot.com",
-  messagingSenderId: "228402277857",
-  appId: "1:228402277857:web:dc65944bf34bba3283f6a0",
-  measurementId: "G-8NY7WBQHEE"
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyB582KCnKC92sag4TOFWlk_7tNH1mAK4mk",
+//   authDomain: "personal-website-323f6.firebaseapp.com",
+//   projectId: "personal-website-323f6",
+//   storageBucket: "personal-website-323f6.appspot.com",
+//   messagingSenderId: "228402277857",
+//   appId: "1:228402277857:web:dc65944bf34bba3283f6a0",
+//   measurementId: "G-8NY7WBQHEE"
+// };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Jeremy's Progress: What exactly is a component?
-        </p>
-        <p>
-          Also: Hi mom and dad!
-        </p>
-        <a
-          className="App-link"
-          href="https://github.com/jkintana/personal-website"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          View this project on GitHub!
-        </a>
-      </header>
-    </div>
+    <Project />
   );
 }
 
---
+++ src/Project.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Project() {
+  return (
+    <div>
+      This is a project.
+    </div>
+  )
+}
+
+export default Project;
