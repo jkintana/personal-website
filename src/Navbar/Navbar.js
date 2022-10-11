02f88a96183ddec4ff6450330c91bfb39557a32a
jkintana <jkintana@protonmail.com>
2022-10-11 14:37:43 -0500

    added a cool, modern, and hip 404 page

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -2,13 +2,13 @@ import React from 'react'
 
 export default function Navbar() {
   return (
-    <>
+    <div>
       <ul class='navbar'>
         <li><a href='https://jkintana.net'>Home</a></li>
         <li><a href='https://jkintana.net'>Portfolio</a></li>
         <li><a href='https://jkintana.net'>Blog</a></li>
         <li><a href='https://github.com/jkintana'>GitHub</a></li>
       </ul>
-    </>
+    </div>
   )
 }
